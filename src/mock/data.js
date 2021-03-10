d9e19f9afb651de8300a5b908cdca5e83e57b7cc
Amine Zeghmi <amin@MacBook-Pro-de-Amine.local>
2021-03-10 12:52:53 +0100

    modified text Hypertube

--- src/mock/data.js
+++ src/mock/data.js
@@ -52,7 +52,7 @@ export const projectsData = [
     img: 'Hypertube.jpg',
     title: 'Hypertube',
     info:
-      'Ce projet vous propose de créer une application web permettant à un utilisateur de rechercher et visionner des vidéos. Le lecteur sera directement intégré au site, et les vidéos seront téléchargées au travers du protocole BitTorrent. Le moteur de recherche interrogera plusieurs sources externes de votre choix. Une fois un élément sélectionné, il sera téléchargé sur le serveur et diffusé sur le player web en même temps. Autrement dit, le lecteur ne se contentera pas d’afficher la vidéo une fois le téléchargement complété, mais sera capable de streamer directement le flux.',
+      'Création dune application web permettant à un utilisateur de rechercher et visionner des vidéos. Le lecteur est intégré au site, et les vidéos sont téléchargées au travers du protocole BitTorrent. Le moteur de recherche interroge plusieurs sources externes. Une fois un élément sélectionné, il sera téléchargé sur le serveur et diffusé sur le player web en même temps. Autrement dit, le lecteur ne se contente pas d’afficher la vidéo une fois le téléchargement complété, mais est capable de streamer directement le flux.',
     info2: 'ReactJS, NodeJS, ExpressJS, SQL',
     url: '',
     repo: 'https://github.com/azeghmi/Hypertube.git',
